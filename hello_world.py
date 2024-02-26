04bed39d2b3051ac159b308dd56d47e87e6f027d
TSK38 <tskahi38@gmail.com>
2024-02-26 15:50:44 +0000

    IssueID123 has been fixed

--- hello_world.py
+++
@@ -1,8 +0,0 @@
-'''This is the pseudo code to Practical Task 1
-Ask the user to input their name and then print out the name
-Use the same approach (input + print) to obtain and print the users age
-At last, print the phrase "Hello World" on a new line'''
-
-name = input("Please enter your name: ")
-age = input("Please enter your age: ")
-print("Hello World!")
