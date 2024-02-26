7a1d377fa605e675ff206f3289c0b9f60acf572c
TSK38 <tskahi38@gmail.com>
2024-02-26 16:11:53 +0000

    IssueID456 has been fixed

--- helloWorld.py
+++ helloWorld.py
@@ -1,2 +1,4 @@
+# The below code creates a variable and asks the user to input a
+# message. It then outputs the message accordingly. 
 user_message = input("Please input a message: ")
 print(user_message)
